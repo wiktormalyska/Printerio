b7175d4fcb30b8c01a97deff423bbb1fbc5e46de
Wiktor Małyska <wiktormalyska03@gmail.com>
2025-07-10 10:50:00 +0200

    Enhance custom order form to create and navigate to order after contract creation; add error handling and toast notifications for user feedback.

--- force-app/main/default/lwc/customOrderLwcForm/customOrderLwcForm.js
+++ force-app/main/default/lwc/customOrderLwcForm/customOrderLwcForm.js
@@ -1,10 +1,9 @@
 
 import { LightningElement,wire } from 'lwc';
 import LightningToast from "lightning/toast";
-import { createRecord } from 'lightning/uiRecordApi';
-import { getObjectInfo, getPicklistValues } from 'lightning/uiObjectInfoApi';
+import { createRecord, deleteRecord } from 'lightning/uiRecordApi';
+import { NavigationMixin } from 'lightning/navigation';
 
-import ORDER_OBJECT from '@salesforce/schema/Order';
 import CONTRACT_OBJECT from '@salesforce/schema/Contract';
 import ACCOUNT_ID_FIELD from '@salesforce/schema/Contract.AccountId';
 import STATUS_FIELD from '@salesforce/schema/Contract.Status';
@@ -17,12 +16,23 @@ import BILLING_STATE_FIELD from '@salesforce/schema/Contract.BillingState';
 import BILLING_POSTAL_CODE_FIELD from '@salesforce/schema/Contract.BillingPostalCode';
 import BILLING_COUNTRY_FIELD from '@salesforce/schema/Contract.BillingCountry';
 
-import SHIPPING_STREET_FIELD from '@salesforce/schema/Contract.ShippingStreet';
-import SHIPPING_CITY_FIELD from '@salesforce/schema/Contract.ShippingCity';
-import SHIPPING_STATE_FIELD from '@salesforce/schema/Contract.ShippingState';
-import SHIPPING_POSTAL_CODE_FIELD from '@salesforce/schema/Contract.ShippingPostalCode';
-import SHIPPING_COUNTRY_FIELD from '@salesforce/schema/Contract.ShippingCountry';
-export default class CustomOrderLwcForm extends LightningElement {
+import ORDER_OBJECT from '@salesforce/schema/Order';
+import ORDER_STATUS_FIELD from '@salesforce/schema/Order.Status';
+import ORDER_ACCOUNT_ID_FIELD from '@salesforce/schema/Order.AccountId';
+import ORDER_EFFECTIVE_DATE_FIELD from '@salesforce/schema/Order.EffectiveDate';
+import ORDER_CONTRACT_ID_FIELD from '@salesforce/schema/Order.ContractId';
+import ORDER_SHIPPING_STREET_FIELD from '@salesforce/schema/Order.ShippingStreet';
+import ORDER_SHIPPING_CITY_FIELD from '@salesforce/schema/Order.ShippingCity';
+import ORDER_SHIPPING_STATE_FIELD from '@salesforce/schema/Order.ShippingState';
+import ORDER_SHIPPING_POSTAL_CODE_FIELD from '@salesforce/schema/Order.ShippingPostalCode';
+import ORDER_SHIPPING_COUNTRY_FIELD from '@salesforce/schema/Order.ShippingCountry';
+import ORDER_BILLING_STREET_FIELD from '@salesforce/schema/Order.BillingStreet';
+import ORDER_BILLING_CITY_FIELD from '@salesforce/schema/Order.BillingCity';
+import ORDER_BILLING_STATE_FIELD from '@salesforce/schema/Order.BillingState';
+import ORDER_BILLING_POSTAL_CODE_FIELD from '@salesforce/schema/Order.BillingPostalCode';
+import ORDER_BILLING_COUNTRY_FIELD from '@salesforce/schema/Order.BillingCountry';
+
+export default class CustomOrderLwcForm extends NavigationMixin(LightningElement) {
 
     //Order Fields
     startDate;
@@ -94,6 +104,25 @@ export default class CustomOrderLwcForm extends LightningElement {
 
         let contract = await this.createContract();
         console.log(contract.id);
+        let order = await this.createOrder(contract.id);
+        console.log(order.id);
+
+        try {
+            this[NavigationMixin.Navigate]({
+                type: 'standard__recordPage',
+                attributes: {
+                    recordId: order.id,
+                    actionName: 'view'
+                }
+            });
+        } catch (error) {
+            await LightningToast.show({
+                label: 'Error',
+                message: `Error navigating to order: ${JSON.stringify(error)}`,
+                variant: 'error',
+                mode: 'sticky'
+            }, this);
+        }
     }
 
     async createContract() {
@@ -123,6 +152,47 @@ export default class CustomOrderLwcForm extends LightningElement {
             }, this);
         }
         return contract;
+    }
+
+    async createOrder(contractId) {
+        let order = null;
+
+        const fields = {};
+        fields[ORDER_STATUS_FIELD.fieldApiName] = "Draft";
+        fields[ORDER_ACCOUNT_ID_FIELD.fieldApiName] = this.accountId;
+        fields[ORDER_EFFECTIVE_DATE_FIELD.fieldApiName] = this.startDate;
+        fields[ORDER_CONTRACT_ID_FIELD.fieldApiName] = contractId;
+        fields[ORDER_SHIPPING_STREET_FIELD.fieldApiName] = this.shippingAddress.street;
+        fields[ORDER_SHIPPING_CITY_FIELD.fieldApiName] = this.shippingAddress.city;
+        fields[ORDER_SHIPPING_STATE_FIELD.fieldApiName] = this.shippingAddress.state;
+        fields[ORDER_SHIPPING_POSTAL_CODE_FIELD.fieldApiName] = this.shippingAddress.postalCode;
+        fields[ORDER_SHIPPING_COUNTRY_FIELD.fieldApiName] = this.shippingAddress.country;
+        fields[ORDER_BILLING_STREET_FIELD.fieldApiName] = this.billingAddress.street;
+        fields[ORDER_BILLING_CITY_FIELD.fieldApiName] = this.billingAddress.city;
+        fields[ORDER_BILLING_STATE_FIELD.fieldApiName] = this.billingAddress.state;
+        fields[ORDER_BILLING_POSTAL_CODE_FIELD.fieldApiName] = this.billingAddress.postalCode;
+        fields[ORDER_BILLING_COUNTRY_FIELD.fieldApiName] = this.billingAddress.country;
+        
+
+        const recordInput = { apiName: ORDER_OBJECT.objectApiName, fields };
+        try {
+            order = await createRecord(recordInput);
+            await LightningToast.show({
+                label: 'Success',
+                message: 'Order created successfully.',
+                variant: 'success',
+                mode: 'dismissable'
+            }, this);
+        } catch (error) {
+            await deleteRecord(contractId);
+            await LightningToast.show({
+                label: 'Error',
+                message: `Error creating order: ${JSON.stringify(error)}`,
+                variant: 'error',
+                mode: 'sticky'
+            }, this);
+        }
+        return order;
     }
 
     //Helpers
